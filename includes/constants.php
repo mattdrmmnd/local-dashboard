be589f04381f7e0cfb491514503d1094ad841b88
Matt D. <76274222+mattdrmmnd@users.noreply.github.com>
2026-09-11 14:18:00 -0700

    Add to and Update constants.php

--- includes/constants.php
+++ includes/constants.php
@@ -1,6 +1,10 @@
 <h3>CONSTANTS</h3>
 
     <?php
+    // Use Capital letters to define a constant as good practice
+    // Always define constantd at the top of the script
+
+    echo "<h4>--define()</h4>";
     define("PI", 3.14);
     define("IS_ADMIN", true);
 
@@ -9,9 +13,23 @@
 
     echo IS_ADMIN;
 
-    function conts_example() {
+    ///////////////////
+    
+    function const_example() {
         echo PI;
     }
     echo "<br>";
     echo PI;
+
+    echo "<br>";
+    ///////////////////
+
+    echo "<h4>--constant()</h4>";
+
+    // Use constant() only when the name isn't known until runtime
+    $equation = constant("PI");
+
+    echo $equation
     ?>
+
+
